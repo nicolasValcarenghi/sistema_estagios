437fd9bae6b2105174aacee121acb1905978ba50
nicolasValcarenghi <104913950+nicolasValcarenghi@users.noreply.github.com>
2024-03-06 19:34:33 +0000

    excluindo arquivos

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,23 +1,24 @@
-<?php return array(
-    'root' => array(
-        'name' => '__root__',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => 'd50c319be03fcf2a0490d49f214cc1a43ba6056f',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'dev' => true,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        '__root__' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => 'd50c319be03fcf2a0490d49f214cc1a43ba6056f',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
+    'reference' => '9cdc5320f6792e5aeb3be9d83de1d0e0a8b8ce9f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9cdc5320f6792e5aeb3be9d83de1d0e0a8b8ce9f',
     ),
+  ),
 );
